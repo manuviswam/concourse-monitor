b2be71f0fd3346cf7b751a0473607c4c650ccff4
Manu Viswam <evmanu@gmail.com>
2017-09-11 23:56:49 +0200

    build task for go, with static fileserver

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
 
 func main()  {
 	fmt.Println("Hello world")
+  http.Handle("/", http.FileServer(http.Dir("dist")))
+  log.Fatal(http.ListenAndServe(":8000", nil))
 }
