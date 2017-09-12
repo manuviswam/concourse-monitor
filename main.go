fea8b3708363680fab951b03f6f7e00e7a278f05
Manu Viswam <evmanu@gmail.com>
2017-09-12 22:35:03 +0200

    Simple socket implementation

--- main.go
+++ main.go
@@ -4,10 +4,29 @@ import (
   "fmt"
   "net/http"
   "log"
+
+  "github.com/gorilla/websocket"
 )
 
-func main()  {
-	fmt.Println("Hello world")
+func main() {
+  http.HandleFunc("/ws", wsHandler)
   http.Handle("/", http.FileServer(http.Dir("dist")))
   log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+  conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+  if err != nil {
+    http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+  }
+
+  go echo(conn)
+}
+
+func echo(conn *websocket.Conn) {
+  for {
+    if err := conn.WriteJSON("Hello world"); err != nil {
+      fmt.Println(err)
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {PipelineStatus} from "../model/pipeline-status";
+import * as Rx from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,42 @@ export class AppComponent {
   pipelineStatuses: PipelineStatus[];
 
   constructor() {
+    let messageObserver = this.connect("ws://localhost:8000/ws");
+    messageObserver.subscribe((val)=> {
+      console.log(val)
+    });
     this.pipelineStatuses = [];
     for (let i =0; i<10 ; i++) {
       this.pipelineStatuses.push(new PipelineStatus("id" +i, "Pipeline "+i, i%3 == 0, i));
     }
+  }
+
+  private socket: Rx.Subject<any>;
+
+  public connect(url): Rx.Subject<any> {
+    if(!this.socket) {
+      this.socket = this.create(url);
+    }
+    return this.socket;
+  }
+
+  private create(url): Rx.Subject<any> {
+    let ws = new WebSocket(url);
+    let observable = Rx.Observable.create(
+      (obs: Rx.Observer<any>) => {
+        ws.onmessage = obs.next.bind(obs);
+        ws.onerror = obs.error.bind(obs);
+        ws.onclose = obs.complete.bind(obs);
+        return ws.close.bind(ws);
+      }
+    );
+    let observer = {
+      next: (data: Object) => {
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(data));
+        }
+      },
+    };
+    return Rx.Subject.create(observer, observable);
   }
 }
